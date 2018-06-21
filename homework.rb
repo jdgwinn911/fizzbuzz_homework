16dcf0824ee76d19bbd8ac0e63fc5863d5da2da1
Josh Gwinn <jdgwinn911@gmail.com>
2018-06-21 11:03:27 -0400

    5th failure

--- homework.rb
+++ homework.rb
@@ -4,14 +4,23 @@ def fizzbuzz()
     loop do 
         f += 1
         fizzy << f 
+        if fizzy[f - 1] % 3 == 0
+            t = f 
+            f = "mined"
+            fizzy.insert(t, f)
+            fizzy.delete(t)
+            f = t
+        elsif fizzy[f - 1] %5 ==0
+            t = f
+            f = "minds"
+            fizzy.insert(t, f)
+            fizzy.delete(t)
+            f = t 
+        end
         if f == 100
           break
         end
     end
-    if fizzy[2] % 3 == 0
-        f = "mined"
-        fizzy.insert(2, f)
-        fizzy.delete(3)
-    end
+   
     fizzy 
 end

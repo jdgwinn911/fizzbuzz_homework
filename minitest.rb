786ab5103cebcb7451abcc90966498ef0538d0a8
Josh Gwinn <jdgwinn911@gmail.com>
2018-06-21 09:21:27 -0400

    second fail

--- minitest.rb
+++ minitest.rb
@@ -6,7 +6,10 @@ class FizzBuzz < Minitest::Test
     def test_assert_that_1_equals_1
         assert_equal(1, 1)
     end
-    def test_array_exists
+    def test_return_a_function_as_array
         assert_equal(Array, fizzbuzz().class)
+    end
+    def test_if_first_array_number_is_1
+        assert_equal(1,fizzbuzz[0])
     end
 end
